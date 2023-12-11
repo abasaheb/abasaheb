3d9252a1c40c13820459fa8484587581037c1a54
Abasaheb Bhingardeve <abasaheb.bhingardeve36@gmail.com>
2023-12-11 20:55:18 +0530

    Simple conditional statement program
    Simple conditional statement program

---
+++ Boolean.java
@@ -0,0 +1,17 @@
+package booleandemo1;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        boolean isSubscribe = false;
+        
+        if (isSubscribe) {
+        	System.out.println("Thanks for requesting!");
+        }
+        else {
+        	System.out.println("Not requested to subscribe!");
+        }
+	}
+
+}
